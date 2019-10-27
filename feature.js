ece2af67bf98620d2e6d165be418ea2a40627a04
Dennis247 <denghazi619@gmail.com>
2019-10-27 14:06:31 +0100

    final master commit

--- feature.js
+++ feature.js
@@ -1,6 +1,5 @@
 boysname = ["andrew", "paul"];
-<<<<<<< HEAD
 console.log("This is the master branch");
-=======
 Console.log("This is branchOne");
->>>>>>> branch1
+console.log("i just made branch 4");
+console.log("final master commit");
